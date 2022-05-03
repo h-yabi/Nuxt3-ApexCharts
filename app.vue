a1033168e0563c705500d499b7d3b9af7a5b84a0
h_yabiku <>
2022-05-03 17:30:49 +0900

    dayjs, chart.js インストール

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
   <div v-if="isLoading">Loading…</div>
-  <div v-else>{{ data }}</div>
-  {{ getDate }}
+  <div v-else>
+    <DoughnutChart v-bind="doughnutChartProps" />
+    {{ data }}
+  </div>
+  <!-- {{ getDate }} -->
 </template>
 
 <script setup lang="ts">
@@ -9,9 +12,51 @@ import { ref, onMounted } from 'vue';
 import dayjs from 'dayjs';
 import axios from 'axios';
 
+import { DoughnutChart, useDoughnutChart } from 'vue-chart-3';
+import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
 const now = dayjs();
 const getDate = dayjs().subtract(1, 'days').format('YYYY-MM-DD');
 console.log(getDate);
+
+const dataValues = ref([30, 40, 60, 70, 5]);
+const dataLabels = ref(['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre']);
+const toggleLegend = ref(true);
+
+const testData = computed<ChartData<'doughnut'>>(() => ({
+  labels: dataLabels.value,
+  datasets: [
+    {
+      data: dataValues.value,
+      backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+    },
+  ],
+}));
+
+const options = computed<ChartOptions<'doughnut'>>(() => ({
+  scales: {
+    myScale: {
+      type: 'logarithmic',
+      position: toggleLegend.value ? 'left' : 'right',
+    },
+  },
+  plugins: {
+    legend: {
+      position: toggleLegend.value ? 'top' : 'bottom',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Doughnut Chart',
+    },
+  },
+}));
+
+const { doughnutChartProps, doughnutChartRef } = useDoughnutChart({
+  chartData: testData,
+  options,
+});
 
 const data = ref([]);
 const country = 'japan';
