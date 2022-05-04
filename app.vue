8a16592c35551cf378942a595c5b0d44e01912e8
h_yabiku <>
2022-05-04 09:54:43 +0900

    一旦コミット

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
   <div v-if="isLoading">Loading…</div>
   <div v-else>
-    <BarChart v-bind="barChartProps" />
-    {{ data }}
+    <!-- <BarChart v-bind="barChartProps" /> -->
+    <LineChart v-bind="lineChartProps" />
+    {{ infectedValues }}
+    <hr />
+    {{ recoveredValues }}
+    <hr />
+    {{ deadValues }}
   </div>
   <!-- {{ getDate }} -->
 </template>
@@ -13,7 +18,7 @@ import dayjs from 'dayjs';
 import axios from 'axios';
 import countryJson from './types/country.json';
 
-import { BarChart, useBarChart } from 'vue-chart-3';
+import { BarChart, LineChart, useBarChart, useLineChart } from 'vue-chart-3';
 import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
 Chart.register(...registerables);
 
@@ -40,7 +45,7 @@ onMounted(async () => {
   isLoading.value = true;
   const apiData = await axios.get(`/api/${country}`);
   data.value = apiData.data;
-  infectedValues.value = apiData.data.map((d: COUNTRY_TYPE) => d.Confirmed);
+  infectedValues.value = apiData.data.map((d: COUNTRY_TYPE) => d.Active);
   recoveredValues.value = apiData.data.map((d: COUNTRY_TYPE) => d.Recovered);
   deadValues.value = apiData.data.map((d: COUNTRY_TYPE) => d.Deaths);
   date.value = apiData.data.map((d: COUNTRY_TYPE) =>
@@ -53,28 +58,35 @@ onMounted(async () => {
 const now = dayjs();
 const getDate = dayjs().subtract(1, 'days').format('YYYY-MM-DD');
 console.log(getDate);
+const toggleLegend = ref(true);
 
 // const date = ref(['感染者数', '回復者数', '死亡者数']);
 
-const testData = computed<ChartData<'bar'>>(() => ({
+const testData = computed<ChartData<'line'>>(() => ({
   labels: date.value,
   datasets: [
     {
       data: infectedValues.value,
-      backgroundColor: ['rgba(0, 0, 255, 0.5)'],
+      backgroundColor: 'rgba(0, 0, 255, 0.5)',
     },
     {
       data: recoveredValues.value,
-      backgroundColor: ['#008080'],
+      backgroundColor: '#008080',
     },
     {
       data: deadValues.value,
-      backgroundColor: ['rgba(255, 0, 0, 0.5)'],
+      backgroundColor: 'rgba(255, 0, 0, 0.5)',
     },
   ],
 }));
 
-const options = computed<ChartOptions<'bar'>>(() => ({
+const options = computed<ChartOptions<'line'>>(() => ({
+  scales: {
+    myScale: {
+      type: 'logarithmic',
+      position: toggleLegend.value ? 'left' : 'right',
+    },
+  },
   plugins: {
     legend: {
       display: false,
@@ -86,7 +98,7 @@ const options = computed<ChartOptions<'bar'>>(() => ({
   },
 }));
 
-const { barChartProps, barChartRef } = useBarChart({
+const { lineChartProps, lineChartRef } = useLineChart({
   chartData: testData,
   options,
 });
